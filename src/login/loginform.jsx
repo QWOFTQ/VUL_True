e7ecd18f62fafddea4eef0ac42aa1d8f12eeb131
QWOFTQ <rkdcofls3890@naver.com>
2024-04-29 22:50:24 +0900

    loginUpdate

--- src/login/loginform.jsx
+++ src/login/loginform.jsx
@@ -1,14 +1,44 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "../App.css";
 
-function loginform() {
+function LoginForm() {
   return (
     <div>
-      <div className="loginform">d</div>
-      <div className="loginform">d</div>
-      <div className="loginform">d</div>
+    <div className="loginform">
+      <div className="box">
+        {/* Email 입력란 */}
+        <div>
+          <label htmlFor="email"></label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Email"
+            required
+          />
+        </div>
+        {/* Password 입력란 */}
+        <div>
+          <label htmlFor="password"></label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Password"
+            required
+          />
+        </div>
+        {/* 로그인 버튼 */}
+        <div>
+          <button type="submit" className="login-button">로그인</button>
+        </div>
+        {/* 회원가입 안내 */}
+        <div className="sign-up"><Link to="/signup">회원가입</Link></div>
+      </div>
     </div>
+  </div>
   );
 }
 
-export default loginform;
+export default LoginForm;
